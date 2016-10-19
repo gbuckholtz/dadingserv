8630c82ad365188b38d8cb6bb715d86969dcd4ef
Glenn Buckholtz <glenn.buckholtz@sympatico.ca>
2016-10-18 20:52:15 -0400

    Initial build.
    web socket sample code

---
+++ src/java/org/mario/websocket/dadingWSEndpoint.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.mario.websocket;
+
+import java.io.IOException;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+import javax.websocket.EncodeException;
+import javax.websocket.OnClose;
+import javax.websocket.OnMessage;
+import javax.websocket.OnOpen;
+import javax.websocket.Session;
+import javax.websocket.server.ServerEndpoint;
+import org.mario.Figure;
+import org.mario.FigureDecoder;
+import org.mario.FigureEncoder;
+
+/**
+ *
+ * @author gbuckholtz
+ */
+@ServerEndpoint(value="/dingit", encoders = {FigureEncoder.class}, decoders = {FigureDecoder.class})
+public class dadingWSEndpoint {
+
+        private static final Set<Session> peers = Collections.synchronizedSet(new HashSet<Session>());
+
+    @OnMessage
+    public void broadcastFigure(Figure figure, Session session) throws IOException, EncodeException {
+        System.out.println("broadcastFigure: " + figure);
+        for (Session peer : peers) {
+            if (!peer.equals(session)) {
+                peer.getBasicRemote().sendObject(figure);
+            }
+        }
+    }
+
+   @OnOpen
+    public void onOpen (Session peer) {
+        peers.add(peer);
+    }
+
+    @OnClose
+    public void onClose (Session peer) {
+        peers.remove(peer);
+    }
+    
+}
